f66eee3f5bb34031010d1295f8ac0e685155b660
kwamekert <tomriddle477@gmail.com>
2020-02-17 15:32:00 +0000

    api host environment added

--- src/app/modules/client-portal/shared/category.service.ts
+++ src/app/modules/client-portal/shared/category.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
+import {environment} from '../../../../environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+
+  public baseUrl = environment.api_host;
 
   constructor(private _httpClient: HttpClient) { }
 
 
 
   getCategoryData(){
-    return this._httpClient.get<any>("http://localhost/helpDesk/public/api/v1/category");
+    return this._httpClient.get<any>(`${this.baseUrl}/v1/category`);
   }
 }
